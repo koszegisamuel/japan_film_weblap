1b935a9aeb29b7aeefd27e86be752992c57b3797
koszegisamuel <koszegi.samuel@gmail.com>
2023-02-22 21:51:04 +0100

    Uploading basic structure

---
+++ app.js
@@ -0,0 +1,33 @@
+const firstVid = document.getElementById("first-video")
+const secondVid = document.getElementById("second-video")
+const thirdVid = document.getElementById("third-video")
+const vidContainer = document.getElementById("video-holder")
+
+const videos = [firstVid, secondVid, thirdVid];
+
+console.log(videos)
+
+let i = Math.floor(Math.random() * videos.length)
+
+isWatched = JSON.parse(localStorage.getItem('isWatched')) || false;
+
+
+console.log(i);
+function playVideo() {
+    videos[i].style.display = "block";
+    videos[i].play();
+
+    videos[i].addEventListener("ended", () => {
+        vidContainer.style.display = "none";
+        isWatched = true;
+        localStorage.setItem('isWatched', JSON.stringify(isWatched));   
+    })
+}
+
+
+if (!isWatched) {
+    playVideo();
+} else {
+    vidContainer.style.display = "none";
+}
+
